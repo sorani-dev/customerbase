469795655bb4ec2dda5d6b3c1daafcbcf62e8c0b
sorani-dev <sorani.dev@outlook.com>
2021-12-03 16:34:35 +0100

    feat: add fetch data from Json Server with Axios

--- schema.js
+++ schema.js
@@ -1,12 +1,14 @@
+const { default: axios } = require('axios')
 const { query } = require('express')
 const { buildSchema, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } = require('graphql')
 
+const BASE_URL = 'http://localhost:3000/customers'
 // Hardcoded data
-const customers = [
-    { id: '1', name: 'John Doe', email: 'jdoe@invalid.com', age: 35 },
-    { id: '2', name: 'Steve Smith', email: 'steves@invalid.com', age: 25 },
-    { id: '3', name: 'Sarah Williams', email: 'swill@invalid.com', age: 32 }
-]
+// const customers = [
+//     { id: '1', name: 'John Doe', email: 'jdoe@invalid.com', age: 35 },
+//     { id: '2', name: 'Steve Smith', email: 'steves@invalid.com', age: 25 },
+//     { id: '3', name: 'Sarah Williams', email: 'swill@invalid.com', age: 32 }
+// ]
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -29,16 +31,29 @@ const RootQuery = new GraphQLObjectType({
                 id: { type: GraphQLString }
             },
             resolve(parentValue, args) {
-                for (let i = 0; i < customers.length; i++) {
-                    if (customers[i].id === args.id) {
-                        return customers[i]
-                    }
-                }
+                // for (let i = 0; i < customers.length; i++) {
+                //     if (customers[i].id === args.id) {
+                //         console.log(customers[i]);
+                //         return customers[i]
+                //     }
+                // }
+
+                return axios.get(`${BASE_URL}/${args.id}`)
+                    .then(res => {
+                        return res.data
+                    })
+                // .catch(err => ([]))
             }
         },
         customers: {
             type: new GraphQLList(CustomerType),
             resolve() {
+                axios.get(`${BASE_URL}/`)
+                    .then(res => {
+                        const data = res.data
+                        console.log(data);
+                        return data
+                    })
                 return customers
             }
         }
